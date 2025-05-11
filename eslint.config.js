ead1b2376957ca2e243ca237949350ee6868e6b4
AshSensei <an9simbol@gmail.com>
2025-05-11 16:49:49 -0700

    syntax change

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,25 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
-import eslintPluginJest from 'eslint-plugin-jest';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import eslintPluginJest from 'eslint-plugin-jest'
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: {
       js,
-      jest: eslintPluginJest,  
-    },  
-    extends: ['js/recommended'], 
+      jest: eslintPluginJest,
+    },
+    extends: ['js/recommended'],
   },
   {
     files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
       globals: {
-        ...globals.browser,  
-        ...globals.node,     
-        ...globals.jest,     
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
       },
     },
   },
-]);
+])
