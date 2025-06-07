debfada1e45ccd4ba77cff426272eb27289cec52
AshSensei <an9simbol@gmail.com>
2025-06-06 19:10:52 -0700

    expanded navbar to be a custom component

--- navbar.js
+++ navbar.js
@@ -1,9 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const nav = document.createElement("nav");
-  nav.innerHTML = `
-    <div class="navbar">
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    const nav = document.createElement('nav');
+    nav.setAttribute('role', 'navigation');
+    nav.setAttribute('aria-label', 'Main Navigation');
+
+    nav.innerHTML = `
       <a href="index.html">Home</a>
-    </div>
-  `;
-  document.body.prepend(nav);
-});
+      <a href="decks.html">Decks</a>
+      <a href="upload.html">Upload</a>
+    `;
+
+    this.shadowRoot.append(nav);
+  }
+}
+
+customElements.define('nav-bar', NavBar);
